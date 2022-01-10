3dc1f220fc1cadea37ab13bd437670c8f973d8ad
ssk801 <77089746+ssk801@users.noreply.github.com>
2022-01-10 12:00:31 +1100

    Update data-strip.py

--- data-strip.py
+++ data-strip.py
@@ -5,9 +5,6 @@ SD card in a pH/conductivity controller
 the values in each daily csv are averaged and appened to a new csv file
 helpfully, the desired data is always in characters 21-26, starting on line 4
 
-script fails if there are blank lines in a data csv
-lacking an elegant solution, simply delete the blank lines then re-run
-
 useful ideas for coding:
     opening, reading and writing from files
     using glob to return files in a path
@@ -17,10 +14,11 @@ sam karpiniec
 02jul2020 v0.1 first version - does the job
 07jul2020 v0.2 moved open cmds outside loop; added column titles
                added sorted() around filename list
+10jan2022 v0.3 added line length check while parsing csv; should prevent blankline fail
 '''
 
 import glob
-list=open('C:/tmp/avg_2020_q4.csv','a+')
+list=open('C:/tmp/avg_2021_q4.csv','a+')
 list.write('filename,conductivity,\n')
 
 for datafilez in sorted(glob.iglob('c:/tmp/test/*.CSV')):
@@ -31,8 +29,12 @@ for datafilez in sorted(glob.iglob('c:/tmp/test/*.CSV')):
 
     values=[]
 
+    #add each value to list, sliced from input data
+    #skip if line length is 1 (i.e. blank line)
+    #normal line length is 74
     for x in range(3,len(lines)):
-        values.append(float(lines[x][20:26]))
+        if len(lines[x])!=1:
+            values.append(float(lines[x][20:26]))
 
     avg=sum(values)/len(values)
 
